48a603519fb7ba2b8e31d3fc530938c84a4c161a
Markus <markus@blaumond.net>
2019-10-25 01:32:07 +0200

    Updated readme and crate level documentation.

--- README.md
+++ README.md
@@ -11,31 +11,47 @@ Add the following to your `cargo.toml` file.
 read-tree = "0.1"
 ```
 
-Trees are created using the builder struct `Sapling`. Nodes can be attached to
-a sapling by using `push(_)`. When a node is added to a sapling it is also
-selected as the parent for later nodes. To finish a node and select its parent
-use `pop()`. When adding a node with no children `push_leaf(_)` can be used;
-it behaves the same as `push(_); pop();`.
+# Description
 
-All changes to a sapling return a `Result`. This is done because the sapling
-ensures certain qualities of your tree. For instance it ensures there is
-exactly one root.
+This crate provides a library for creating and then querying trees.
+The trees are not intended to be modified after their initial creation.
 
-When the sapling is complete you can use `build()` to turn the sapling into
-a `Tree`. The resulting tree can no longer be modified. Navigating trees is
-done by using slices of trees called `Node`s. To get started use `root()` on a
-tree to get its root node.
+Internally trees are represented by a `Vec<Vertex<T>>` where each vertex carries the payload of that node in the tree and the number of its descendants.
+In addition the vertices are sorted depth first; meaning every vertex is followed by the vertex of its first child.
+Using the length of a vertex we can easily skip a nodes entire subtree and can instantly access its sibling.
+
+Slicing a tree into a node is as simple as slicing the trees vertices buffer into a `&[Vertex<T>]`.
+We wrap this slice in a `Node<T>`.
+
+# Examples
+
+Trees are created using `Sapling`s.
+Nodes can be attached to a sapling by using `push`.
+When a node is added to a sapling it is also selected as the parent for nodes that are added later.
+To finish a node and reselect its parent call `pop`.
+When adding a node with no children use `push_leaf`.
+There are more methods to push other saplings, trees or even nodes.
+See `Sapling` for more information.
+
+When the sapling is complete, you can `build` it into a `Tree<T>`.
+The resulting tree can no longer be modified.
+Navigating trees is done by using slices of trees called `Node<T>`.
+To get started use `as_node` on a tree to get its root node which represents the entire tree.
 
 Nodes support various iterators to navigate their contents.
 
 ```rust
-use read-tree::Sapling;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use read_tree::Sapling;
 
-let sap = Sapling::new();
-sap.push(1).unwrap();
+    let mut sap = Sapling::new();
+    sap.push(1);
+    sap.pop();
 
-let tree = sap.build().unwrap();
-let root = tree.root();
+    let tree = sap.build()?;
+    let root = tree.as_node();
 
-assert_eq!(root.data, 1);
+    assert_eq!(root.data(), &1);
+    Ok(())
+}
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,53 @@
 //! This crate provides a library for creating and then querying trees. The
 //! trees are not intended to be modified after their initial creation.
 //!
-//! Internally trees are represented by a [Vec] of vertices where each vertex
-//! carries the payload of that node in the tree and the number of nodes in the
-//! subtree of the node, called the length of the vertex. In addition the
-//! vertices are sorted depth first; meaning every vertex is followed by the
-//! vertex of its first child. Using the length of a vertex we can easily skip a
-//! nodes entire subtree and can instantly access its sibling.
+//! Internally trees are represented by a [`Vec`]`<`[`Vertex`]`<T>>` where each
+//! vertex carries the payload of that node in the tree and the number of its
+//! descendants. In addition the vertices are sorted depth first; meaning every
+//! vertex is followed by the vertex of its first child. Using the length of a
+//! vertex we can easily skip a nodes entire subtree and can instantly access
+//! its sibling.
 //!
-//! Slicing a tree into a node is as simple as slicing the trees [Vec] of
-//! vertices into a `&[Vertex<T>]` slice.
+//! Slicing a tree into a node is as simple as slicing the trees vertices buffer
+//! into a `&[`[`Vertex`]`<T>]`. We wrap this slice in a [`Node`]`<T>`.
 //!
-//! # Example
+//! # Examples
 //!
-//! Trees are created using the builder struct [Sapling][Sapling]. Nodes can be
-//! attached to a sapling by using `.push(_)`. When a node is added to a sapling
-//! it is also selected as the parent for later nodes. To finish a node and
-//! reselect its parent use `.pop()`. When adding a node with no children use
-//! `.push_leaf(_)`; it behaves the same as `.push(_); .pop();`.
+//! Trees are created using [`Sapling`]s. Nodes can be attached to a sapling by
+//! using [`push`]. When a node is added to a sapling it is also selected as the
+//! parent for nodes that are added later. To finish a node and reselect its
+//! parent call [`pop`]. When adding a node with no children use [`push_leaf`].
+//! There are more methods to push other saplings, trees or even nodes. See
+//! [`Sapling`] for more information.
 //!
-//! When the sapling is complete, you can use `.build()` to turn the sapling
-//! into a [Tree][Tree]. The resulting tree can no longer be modified.
-//! Navigating trees is done by using slices of trees called [Node][Node]s. To
-//! get started use `.root()` on a tree to get its root node which contains the
-//! entire tree.
+//! When the sapling is complete, you can [`build`] it into a [`Tree`]`<T>`. The
+//! resulting tree can no longer be modified. Navigating trees is done by using
+//! slices of trees called [`Node`]`<T>`. To get started use [`as_node`] on a
+//! tree to get its root node which represents the entire tree.
 //!
 //! Nodes support various iterators to navigate their contents.
 //!
 //! ```rust
-//! use read_tree::Sapling;
+//! fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     use read_tree::Sapling;
 //!
-//! let mut sap = Sapling::new();
-//! sap.push(1);
-//! sap.pop();
+//!     let mut sap = Sapling::new();
+//!     sap.push(1);
+//!     sap.pop();
 //!
-//! let tree = sap.build().unwrap();
-//! let root = tree.as_node();
+//!     let tree = sap.build()?;
+//!     let root = tree.as_node();
 //!
-//! assert_eq!(root.data(), &1);
+//!     assert_eq!(root.data(), &1);
+//!     Ok(())
+//! }
 //! ```
+//!
+//! [`as_node`]: Tree::as_node
+//! [`build`]: Sapling::build
+//! [`pop`]: Sapling::pop
+//! [`push_leaf`]: Sapling::push_leaf
+//! [`push`]: Sapling::push
 
 mod tree;
 
